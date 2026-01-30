a239d601bbb7bcc1e7e2ece968efc18536b9a629
sanjeev <sanjeev.itprofessioal@gmail.com>
2026-01-30 22:56:57 +0530

    new added is added for this project

---
+++ src/main/java/com/devops/lab/App.java
@@ -0,0 +1,14 @@
+package com.devops.lab;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println("I love you Pooja");
+		
+    }
+}
